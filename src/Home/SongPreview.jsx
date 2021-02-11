77318660cb8c383fc4743668668caaf05fbbe37b
Christina Tkatchenko <christinatka@gmail.com>
2021-02-11 09:30:45 -0500

    tweak

--- src/Home/SongPreview.jsx
+++ src/Home/SongPreview.jsx
@@ -1,11 +1,4 @@
 import { Box, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  title: {
-
-  },
-}));
 
 const SongPreview = ({ nowPlaying }) => {
 
